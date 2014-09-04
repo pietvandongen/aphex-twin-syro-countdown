0e35cd6d9e6ef78813f075524272521801f44986
Piet van Dongen <piet@studio-donder.nl>
2014-09-04 21:40:36 +0200

    Added time since last visit

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,4 @@
+require("jquery.cookie");
 var $ = require("jquery");
 
 var DATE_SEPARATOR = "/";
@@ -17,12 +18,21 @@ var UPDATE_INTERVAL_MINUTES = (UPDATE_INTERVAL_SECONDS * 60) / Math.pow(10, DECI
 var UPDATE_INTERVAL_HOURS = (UPDATE_INTERVAL_MINUTES * 60) / Math.pow(10, DECIMAL_PRECISION_HOURS);
 var UPDATE_INTERVAL_DAYS = (UPDATE_INTERVAL_HOURS * 24) / Math.pow(10, DECIMAL_PRECISION_DAYS);
 
+var MILLISECONDS_IN_A_SECOND = 1000;
+var SECONDS_IN_A_MINUTE = 60;
+var MINUTES_IN_AN_HOUR = 60;
+var HOURS_IN_A_DAY = 24;
+
+var COOKIE_EXPIRATION_DAYS = 365;
+
 var releaseDate;
+var lastVisitDate;
 
 var initialize = function () {
     releaseDate = getReleaseDate();
+    lastVisitDate = getLastVisitDate();
 
-    $("#date-of-release").text(getReleaseDateFormatted());
+    showReleaseDate();
 
     showRemainingTimeInMilliseconds();
     showRemainingTimeInSeconds();
@@ -35,6 +45,24 @@ var initialize = function () {
     updateRemainingTimeInMinutes();
     updateRemainingTimeInHours();
     updateRemainingTimeInDays();
+
+    if (lastVisitDate !== null) {
+        showTimePassedSinceLastVisitInMilliseconds();
+        showTimePassedSinceLastVisitInSeconds();
+        showTimePassedSinceLastVisitInMinutes();
+        showTimePassedSinceLastVisitInHours();
+        showTimePassedSinceLastVisitInDays();
+
+        updateTimePassedSinceLastVisitInMilliseconds();
+        updateTimePassedSinceLastVisitInSeconds();
+        updateTimePassedSinceLastVisitInMinutes();
+        updateTimePassedSinceLastVisitInHours();
+        updateTimePassedSinceLastVisitInDays();
+
+        $('#time-since-last-visit').show();
+    }
+
+    saveCookie();
 };
 
 var getReleaseDate = function () {
@@ -55,24 +83,65 @@ var getReleaseDateFormatted = function () {
         releaseDate.getFullYear();
 };
 
+var getLastVisitDate = function() {
+    var lastVisitTimestamp = $.cookie("lastVisit");
+
+    if (typeof lastVisitTimestamp == "undefined") {
+        return null;
+    }
+    else {
+        return new Date(parseInt(lastVisitTimestamp));
+    }
+};
+
+var saveCookie = function() {
+    $.cookie("lastVisit", new Date().getTime(), {
+        expires: COOKIE_EXPIRATION_DAYS
+    });
+};
+
 var getRemainingTimeInMilliseconds = function () {
     return releaseDate - new Date().getTime();
 };
 
 var getRemainingTimeInSeconds = function () {
-    return getRemainingTimeInMilliseconds() / 1000;
+    return getRemainingTimeInMilliseconds() / MILLISECONDS_IN_A_SECOND;
 };
 
 var getRemainingTimeInMinutes = function () {
-    return getRemainingTimeInSeconds() / 60;
+    return getRemainingTimeInSeconds() / SECONDS_IN_A_MINUTE;
 };
 
 var getRemainingTimeInHours = function () {
-    return getRemainingTimeInMinutes() / 60;
+    return getRemainingTimeInMinutes() / MINUTES_IN_AN_HOUR;
 };
 
 var getRemainingTimeInDays = function () {
-    return getRemainingTimeInHours() / 24;
+    return getRemainingTimeInHours() / HOURS_IN_A_DAY;
+};
+
+var getTimePassedSinceLastVisitInMilliseconds = function () {
+    return new Date().getTime() - lastVisitDate;
+};
+
+var getTimePassedSinceLastVisitInSeconds = function () {
+    return getTimePassedSinceLastVisitInMilliseconds() / 1000;
+};
+
+var getTimePassedSinceLastVisitInMinutes = function () {
+    return getTimePassedSinceLastVisitInSeconds() / 60;
+};
+
+var getTimePassedSinceLastVisitInHours = function () {
+    return getTimePassedSinceLastVisitInMinutes() / 60;
+};
+
+var getTimePassedSinceLastVisitInDays = function () {
+    return getTimePassedSinceLastVisitInHours() / 24;
+};
+
+var showReleaseDate = function () {
+    $("#date-of-release").text(getReleaseDateFormatted());
 };
 
 var showRemainingTimeInMilliseconds = function () {
@@ -107,6 +176,38 @@ var showRemainingTimeInDays = function () {
     );
 };
 
+var showTimePassedSinceLastVisitInMilliseconds = function() {
+    $("#time-since-last-visit-in-milliseconds").text(getTimePassedSinceLastVisitInMilliseconds());
+};
+
+var showTimePassedSinceLastVisitInSeconds = function () {
+    $("#time-since-last-visit-in-seconds").text(Math.round(getTimePassedSinceLastVisitInSeconds()));
+};
+
+var showTimePassedSinceLastVisitInMinutes = function () {
+    $("#time-since-last-visit-in-minutes").text(
+            Math.round(
+                    getTimePassedSinceLastVisitInMinutes() * Math.pow(10, DECIMAL_PRECISION_MINUTES)
+            ) / Math.pow(10, DECIMAL_PRECISION_MINUTES)
+    );
+};
+
+var showTimePassedSinceLastVisitInHours = function () {
+    $("#time-since-last-visit-in-hours").text(
+            Math.round(
+                    getTimePassedSinceLastVisitInHours() * Math.pow(10, DECIMAL_PRECISION_HOURS)
+            ) / Math.pow(10, DECIMAL_PRECISION_HOURS)
+    );
+};
+
+var showTimePassedSinceLastVisitInDays = function () {
+    $("#time-since-last-visit-in-days").text(
+            Math.round(
+                    getTimePassedSinceLastVisitInDays() * Math.pow(10, DECIMAL_PRECISION_DAYS)
+            ) / Math.pow(10, DECIMAL_PRECISION_DAYS)
+    );
+};
+
 var updateRemainingTimeInMilliseconds = function () {
     setInterval(function () {
         showRemainingTimeInMilliseconds();
@@ -134,6 +235,36 @@ var updateRemainingTimeInHours = function () {
 var updateRemainingTimeInDays = function () {
     setInterval(function () {
         showRemainingTimeInDays();
+    }, UPDATE_INTERVAL_DAYS);
+};
+
+var updateTimePassedSinceLastVisitInMilliseconds = function () {
+    setInterval(function () {
+        showTimePassedSinceLastVisitInMilliseconds();
+    }, UPDATE_INTERVAL_MILLISECONDS);
+};
+
+var updateTimePassedSinceLastVisitInSeconds = function () {
+    setInterval(function () {
+        showTimePassedSinceLastVisitInSeconds();
+    }, UPDATE_INTERVAL_SECONDS);
+};
+
+var updateTimePassedSinceLastVisitInMinutes = function () {
+    setInterval(function () {
+        showTimePassedSinceLastVisitInMinutes();
+    }, UPDATE_INTERVAL_MINUTES);
+};
+
+var updateTimePassedSinceLastVisitInHours = function () {
+    setInterval(function () {
+        showTimePassedSinceLastVisitInHours();
+    }, UPDATE_INTERVAL_HOURS);
+};
+
+var updateTimePassedSinceLastVisitInDays = function () {
+    setInterval(function () {
+        showTimePassedSinceLastVisitInDays();
     }, UPDATE_INTERVAL_DAYS);
 };
 
